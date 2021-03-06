cb4a215c738031ae699e17744633b61bc38d902a
suportebeloj <suportebeloj@gmail.com>
2021-03-06 12:38:38 -0300

    detalhes da pagina inicial

--- static/js/puro/modal.js
+++ static/js/puro/modal.js
@@ -2,14 +2,35 @@
 let modal = document.getElementById("banner-modal");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-let img = document.getElementById("banner-image-link");
+let imgs = document.querySelectorAll(".open-modal");
+let banner_image = document.querySelector('#banner-image-link')
 let modalImg = document.getElementById("banner-image-modal");
 let captionText = document.getElementById("caption");
-img.onclick = function () {
+
+banner_image.onclick = function () {
     modal.style.display = "block";
     modalImg.src = this.getAttribute('data-src');
     captionText.innerHTML = this.getAttribute('data-alt');
 }
+
+imgs.forEach(item => {
+    item.onclick = function () {
+        modal.style.display = "block";
+        modalImg.src = this.getAttribute('data-src');
+        captionText.innerHTML = this.getAttribute('data-alt');
+    }
+})
+
+
+
+/**
+ img.onclick = function () {
+     modal.style.display = "block";
+     modalImg.src = this.getAttribute('data-src');
+     captionText.innerHTML = this.getAttribute('data-alt');
+ }
+ * 
+ */
 
 // Get the <span> element that closes the modal
 let span = document.getElementsByClassName("close")[0];
